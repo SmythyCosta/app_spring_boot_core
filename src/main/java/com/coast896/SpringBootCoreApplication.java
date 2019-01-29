157d2d5388fda0e806e98b58820a3c26e3dea706
Smythy Costa <smythy.costa@hotmail.com>
2019-01-29 11:22:39 -0300

    core refactorings

--- src/main/java/com/coast896/SpringBootCoreApplication.java
+++ src/main/java/com/coast896/SpringBootCoreApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringBootJwtAuthenticationApplication {
+public class SpringBootCoreApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJwtAuthenticationApplication.class, args);
+		SpringApplication.run(SpringBootCoreApplication.class, args);
 	}
 	
 	@Bean
